f825c3ab729583d6e674b5586edeb30008997c00
Nitin Pandey <rahulpandey127003@gmail.com>
2025-12-15 18:12:08 +0530

    Fix backend HTTPS

--- fronted/src/axiosConfig.js
+++ fronted/src/axiosConfig.js
@@ -1,4 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://13.62.228.101:3002";
+axios.defaults.baseURL = "https://13.62.228.101";
+axios.defaults.withCredentials = true;
 export default axios;
